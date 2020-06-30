9c84bf772429a72385fc28edec2d01a7310dd173
Andrey Vayner <Endeavour@Andreys-MacBook-Pro.local>
2020-06-30 10:30:35 -0400

    fixed some bugs in qsohostfcn.py

--- common/qsohostfcn.py
+++ common/qsohostfcn.py
@@ -54,6 +54,36 @@ def qso_continuum_legendre(wave,i,j,k):
     legendre_poly = numpy.polynomial.legendre.legval(x,[i,j,k])
     return legendre_poly
 
+def qsotemplate_scale_legendre(wave,qso_model,i,j,k):
+    
+    '''Function defined for fitting a legendre polynomial to continuum
+        
+        Parameters
+        -----
+        wave: array
+        1-D array of wavelengths
+        i,j,k: floats
+        scale factors for first, second and third order legendre polynomials
+        
+        returns
+        -------
+        legendre_poly: array
+        '''
+    
+    
+    
+    #print(kwargs.items())
+    #return numpy.polynomial.legendre.legval(wave,list(kwargs.values()))
+    x=numpy.linspace(0,1,len(wave))
+    legendre_poly = numpy.polynomial.legendre.legval(x,[i,j,k])
+    return qso_model*legendre_poly
+
+
+#def qsotemplate_model_legendre(wave,qso_model,degree,a):
+#    x=numpy.linspace(0,1,len(wave))
+#    return qso_model*numpy.polynomial.legendre.legval(x,[1]*degree)
+
+
 
 def continuum_additive_polynomial(wave,a,b,c,d,e,f,g,h):
     '''Function defined for fitting a continuum with additive polynomials
@@ -80,4 +110,8 @@ def continuum_additive_polynomial(wave,a,b,c,d,e,f,g,h):
     #x2=wave[::-1]
     ymod = b*(numpy.exp(-a*x))+d*(numpy.exp(-c*x2))+f*(1-numpy.exp(-e*x))+h*(1-numpy.exp(-g*x2))
 
+
+
+
+    return ymod
 

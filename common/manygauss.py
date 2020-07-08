4ccfec0905572608ae72656219dc66951d24a582
Yuzo Ishikawa <65511234+yuyuishi12@users.noreply.github.com>
2020-07-08 11:01:22 -0400

    major changes to manygauss: implemented LMFIT Composite Model; can get fit stats
    Some difficulties applying the line-ratios as a separate free parameter in the LMFIT Model mode. Will add patch later. Current code can consistently reproduce fits.

--- common/manygauss.py
+++ common/manygauss.py
@@ -41,11 +41,11 @@
 ;      2016sep26, DSNR, switched to deconvolving resolution in situ by adding
 ;                       resolution in quadrature in wavelength space to each
 ;                       velocity component
-;      2020jun01, YI, translated to Python 3
-;      2020jun22, YI, added LMFIT functions that build the parameter variables 
-;                     and running the fits that call many_gauss()
+;      2020jun01, YI, translated to Python 3 - ver2
+;      2020jun22, YI, added LMFIT functions that build the parameter variables and running the fits that call many_gauss() - ver2
+;      2020jul06, YI, refined the LMFIT parameter set --> able to reproduce MPFIT results, but cannot get uncertainties - ver3
 ;      2020jul07, DSNR, fixed indexing bug in expr
-;      2020jul07, YI, refined the LMFIT parameter set --> able to reproduce MPFIT results, but cannot get uncertainties
+;      2020jul08, YI, complete rewrite: implemented LMFIT Composite Model instead of LMFIT Minimizer for fits; can get fit stats. major changes to manygauss - ver4
 ;    
 ; :Copyright:
 ;    Copyright (C) 2013--2016 David S. N. Rupke
@@ -68,47 +68,43 @@
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from lmfit import Parameters, Minimizer,minimize,fit_report
+from lmfit import Parameters, Minimizer,minimize,fit_report,report_fit,Model
 import re
-import pdb
 
-def lm_resid(param,wave,flux_nocnt,flux_err):
-    """Calculate total residual for fits of Gaussians to several data sets."""
-    parvalue = np.zeros(len(param))
-    for ip, par in enumerate(param.valuesdict()):
-        pValue = param[par].value
-        parvalue[ip] = pValue
-    manyGauss = manygauss(wave,parvalue)
-    resid = (flux_nocnt - manyGauss )/flux_err
-    return resid
-
-def fix_expressions(params,pnames,paramExp,indexs=[0]):
-    # new_exprs = []
-    print('------------------')
+def fix_expressions(pnames,paramExp,indexs=[0],lineRat=[0]):
     # now go through each expressions....
+    newExp = []
     for ex, exp in enumerate(paramExp):
-        exp = re.sub('d','',exp)
-        pind = np.array(re.findall(r'P\[(.*?)\]', exp)).astype(int)
-        nexp = exp
-        for epind in pind:
-            sepind = 'P['+str(epind)+']'
-            nexp = nexp.replace(sepind,pnames[epind])
-        # new_exprs.append(nexp)
-        ni = indexs[ex]
-        params[pnames[ni]].expr = nexp
-    return params
+        if ex in indexs:
+            
+            exp = re.sub('d','',exp)
+            pind = np.array(re.findall(r'P\[(.*?)\]', exp)).astype(int)
+            nexp = exp
+            for epind in pind:
+                sepind = 'P['+str(epind)+']'
+                if epind in lineRat:
+                    nexp = nexp.replace(sepind,str(lineRat[epind]))
+                else:
+                    nexp = nexp.replace(sepind,pnames[epind])
+            newExp.append(nexp)
+        else:
+            newExp.append(exp)
+    return newExp
 
-def set_params(fit_params,NAME=None,VALUE=None,VARY=True,LIMITED=None,TIED=True,LIMITS=None,STEP=None):
+def set_params(fit_params,NAME=None,VALUE=None,VARY=True,LIMITED=None,TIED='',LIMITS=None,STEP=None):
     LIMITED = np.array(LIMITED).astype(int)
-    fit_params.add(NAME, value=VALUE,vary = VARY)
+    fit_params[NAME].set(value=VALUE,vary=VARY)
     if STEP != 0.:
         fit_params[NAME].brute_step=STEP
+    if TIED != '' :
+        fit_params[NAME].expr=TIED
     for li in [0,1]:
         if LIMITED[li] == 1:
             if li == 0:
                 fit_params[NAME].min=LIMITS[0]
             elif li == 1:
                 fit_params[NAME].max=LIMITS[1]
+    
     return fit_params
 
 def run_manygauss(wave,flux_nocnt,flux_err,parinfo,maxiter=1000.):
@@ -118,23 +114,29 @@ def run_manygauss(wave,flux_nocnt,flux_err,parinfo,maxiter=1000.):
     find = ppoff+np.arange(nline)*3
     wind = find + 1
     sind = find + 2
-
-    fit_params = Parameters()
-    # pnames = []
+    
+    manyGauss = []
     parTie = []
+    iparTi = []
+    parnames = []
+    parval = []
+    ilnRat = {}
     
     for ip, par in enumerate(parinfo):
         ptie = parinfo[ip]['tied']
         if isinstance(ptie,bytes):
             ptie = ptie.decode('utf-8')
-            if ptie != '':
-                ptie = ptie.replace(' ','')
-                ptie = ptie.replace('+',' + ').replace('*',' * ').replace('-',' - ').replace('/',' / ')
-                ptie = ptie.replace('E + ','E+')
-                parTie.append(ptie)
-        fixed =bool(int(parinfo[ip]['fixed']))
-        ifixed = not fixed
-        # print(ip,fixed)
+        if ptie != '':
+            ptie = ptie.replace(' ','')
+            ptie = ptie.replace('+',' + ').replace('*',' * ').replace('-',' - ').replace('/',' / ')
+            ptie = ptie.replace('e + ','E + ')
+            ptie = ptie.replace('E + ','E+')
+            iparTi.append(ip)
+        parTie.append(ptie)
+        ival = parinfo[ip]['value']
+        if type(ival) is np.ndarray or isinstance(ival,list):
+            ival = ival[0]
+        parval.append(ival)
         if ip >= find[0]:
             line = parinfo[ip]['line']
             if isinstance(line,bytes):
@@ -143,22 +145,21 @@ def run_manygauss(wave,flux_nocnt,flux_err,parinfo,maxiter=1000.):
                 line = 'blank_'+str(ip)
             else:
                 line = line.replace(' ','_').replace('.','').replace('-','').replace('-','').replace('[','').replace(']','').replace('/','')
-            # print(ip,line)
             if ip in find:
-                fpar = parinfo[ip]
-                fname = 'flx_%s' % (line)
-                fit_params = set_params(fit_params,NAME=fname,VALUE=fpar['value'],VARY=ifixed,LIMITED=fpar['limited'],
-                                        TIED=fpar['tied'],LIMITS=fpar['limits'],STEP=fpar['step'])
+                fname = '%s_flx' % (line)
+                mName = '%s_' % (line)
+                parnames.append(fname)
+                imodel = Model(manygauss,independent_vars=['x'],prefix=mName)
+                if ip == find[0]:
+                    manyGauss = imodel
+                else:
+                    manyGauss += imodel
             elif ip in wind:
-                wpar = parinfo[ip]
-                wname = 'wav_%s' % (line)
-                fit_params = set_params(fit_params,NAME=wname,VALUE=wpar['value'],VARY=ifixed,LIMITED=wpar['limited'],
-                                        TIED=wpar['tied'],LIMITS=wpar['limits'],STEP=wpar['step'])
+                wname = '%s_cwv' % (line)
+                parnames.append(wname)
             elif ip in sind:
-                spar = parinfo[ip]
-                sname = 'sig_%s' % (line)
-                fit_params = set_params(fit_params,NAME=sname,VALUE=spar['value'],VARY=ifixed,LIMITED=spar['limited'],
-                                        TIED=spar['tied'],LIMITS=spar['limits'],STEP=spar['step'])
+                sname = '%s_sig' % (line)
+                parnames.append(sname)
         else:
             parname = parinfo[ip]['parname']
             if isinstance(parname,bytes):
@@ -167,98 +168,133 @@ def run_manygauss(wave,flux_nocnt,flux_err,parinfo,maxiter=1000.):
                 parname = 'blank_'+str(ip)
             else:
                 parname = parname.replace(' ','_').replace('.','').replace('-','').replace('-','').replace('[','').replace(']','').replace('/','_')
-            # pnames.append(parname)
+                if len(parname.split('line_ratio')) > 1:
+                    ilnRat[ip]=parinfo[ip]['value'][0]
             ipar = parinfo[ip]
-            fit_params = set_params(fit_params,NAME=parname,VALUE=ipar['value'],VARY=ifixed,LIMITED=ipar['limited'],
-                                        TIED=ipar['tied'],LIMITS=ipar['limits'],STEP=ipar['step'])
-    # now check for the EXPRESSIONS
-    pnames = list(fit_params.valuesdict())
-    expr = [idx for idx,item in enumerate(parinfo) if item['tied'] != '']
-    print(expr)
-    if len(parTie) != 0:
-        fit_params = fix_expressions(fit_params,pnames,parTie,indexs=expr)
-        
-    lmout = minimize(lm_resid, fit_params, args=(wave,flux_nocnt,flux_err),
-                      method='least_squares',max_nfev=maxiter,calc_covar =True)
+            parnames.append(parname)
+    paramNames = manyGauss.param_names
+    fit_params = manyGauss.make_params()
+    newExp = fix_expressions(parnames,parTie,indexs=iparTi,lineRat=ilnRat)
+    # ppoff = int(parinfo[0]['value'])
+    gind = np.arange(nline)*4.+3
+    ipn = 0
+    ip = 0
+    while ipn < len(paramNames):
+        par = paramNames[ipn]
+        if ipn in gind:
+            ipar = parinfo[2]
+            fit_params = set_params(fit_params,NAME=par,VALUE=ipar['value'],VARY=False,LIMITED=ipar['limited'],
+                                        TIED='',LIMITS=ipar['limits'],STEP=ipar['step'])
+        else:
+            par = paramNames[ipn]
+            ipar = parinfo[ip+ppoff]
+            fixed = bool(int(ipar['fixed']))
+            ifixed = not fixed
+            itied = newExp[ip+ppoff]
+            fit_params = set_params(fit_params,NAME=par,VALUE=ipar['value'],VARY=ifixed,LIMITED=ipar['limited'],
+                                            TIED=itied,LIMITS=ipar['limits'],STEP=ipar['step'])
+            ip+=1
+        ipn+=1
+    lmout = manyGauss.fit(flux_nocnt,fit_params,x=wave,method='least_squares',weights=1/flux_err,max_nfev=maxiter)
+    specfit = manyGauss.eval(lmout.params,x=wave)
+    print('*******************************************************************')
+    print(lmout.fit_report())
+    print('*******************************************************************')
+    # get the param (array in the same format as the IDL mpfit version)
+    pardict = []
+    parOut = parval
+    for ip,par in enumerate(lmout.values):
+        if ip not in gind:
+            pardict.append(lmout.values[par])
+    for ip in range(ppoff,len(parval)):
+        parOut[ip]=pardict[ip-ppoff]
+    # now extract the perror (stddev for each parameter)
+    perror=list(np.zeros(ppoff))
+    for ip,par in enumerate(lmout.params.items()):
+        if ip not in gind:
+            perror.append(par[1].stderr)
     
-    parout = lmout.params
-    parval = np.zeros(len(parinfo))
-    for ip, par in enumerate(parout):
-        parval[ip] = parout[par].value
-    specfit = manygauss(wave,parval)
-    print(fit_report(lmout.params))
-    return lmout,parval,specfit
+    return lmout,parOut,specfit,perror
 
 
-def manygauss(wave,param,specresarr=None):
+def manygauss(x,flx, cwv, sig,srsigslam):
+    # param 0 flux
+    # param 1 central wavelength
+    # param 2 sigma
     c=299792.458
+    sigs = np.sqrt(np.power((sig/c)*cwv,2.) + np.power(srsigslam,2.))
+    gaussian = flx*np.exp(-np.power((x-cwv) / sigs,2.)/2.)
+    return gaussian
 
-    ppoff = param[0].astype(int)
-    nwave = len(wave)
-    nline = ((len(param)-ppoff)/3).astype(int)
+# def manygauss(wave,param,specresarr=None):
+#     c=299792.458
 
-    find = ppoff+np.arange(nline)*3
-    wind = find + 1
-    sind = find + 2
+#     ppoff = param[0].astype(int)
+#     nwave = len(wave)
+#     nline = ((len(param)-ppoff)/3).astype(int)
 
-    dispersion = wave[1] - wave[0]
+#     find = ppoff+np.arange(nline)*3
+#     wind = find + 1
+#     sind = find + 2
+
+#     dispersion = wave[1] - wave[0]
     
     
-    if specresarr:
-        # flipped the (row,column) indices of specresarr
-        # np.searchsorted() works a little differently from value_locate() in IDL
-        wsr = np.searchsorted(specresarr[0,:],param[wind])
-        # switched from [wsr,1] since Python reads [row,column] and IDL reads [column, row]
-        srsigslam = param[wind]/specresarr[1,wsr]/2.35 
-    else:
-        srsigslam = np.zeros(nline)+param[2]
+#     if specresarr:
+#         # flipped the (row,column) indices of specresarr
+#         # np.searchsorted() works a little differently from value_locate() in IDL
+#         wsr = np.searchsorted(specresarr[0,:],param[wind])
+#         # switched from [wsr,1] since Python reads [row,column] and IDL reads [column, row]
+#         srsigslam = param[wind]/specresarr[1,wsr]/2.35 
+#     else:
+#         srsigslam = np.zeros(nline)+param[2]
   
-    # resolution in wavelength space [sigma] assumed to be in third element of PARAM
+#     # resolution in wavelength space [sigma] assumed to be in third element of PARAM
 
-    sigs = np.sqrt(np.power((param[sind]/c)*param[wind],2.) + np.power(srsigslam,2.))
-    maxsig = np.max(sigs)
+#     sigs = np.sqrt(np.power((param[sind]/c)*param[wind],2.) + np.power(srsigslam,2.))
+#     maxsig = np.max(sigs)
     
-    nsubwave = np.round(10. * maxsig / dispersion)
-    halfnsubwave = np.round(nsubwave / 2)
-    nsubwave = (halfnsubwave*2+1).astype(int)
+#     nsubwave = np.round(10. * maxsig / dispersion)
+#     halfnsubwave = np.round(nsubwave / 2)
+#     nsubwave = (halfnsubwave*2+1).astype(int)
     
     
-    # indsubwaves = rebin(transpose(fix(indgen(nsubwave)-halfnsubwave)),nline,nsubwave)
-    # in the Python version, the transpose happens after the np.resize rebinning
-    indsubwaves = np.transpose(np.resize((np.arange(nsubwave)-halfnsubwave).astype(int),[nline,nsubwave])) 
+#     # indsubwaves = rebin(transpose(fix(indgen(nsubwave)-halfnsubwave)),nline,nsubwave)
+#     # in the Python version, the transpose happens after the np.resize rebinning
+#     indsubwaves = np.transpose(np.resize((np.arange(nsubwave)-halfnsubwave).astype(int),[nline,nsubwave])) 
 
-    fluxes = param[find]
-    refwaves = param[wind]
-    indrefwaves_real = (refwaves - wave[0]) / dispersion
-    indrefwaves = (indrefwaves_real).astype(int)
-    indrefwaves_frac = indrefwaves_real - indrefwaves.astype(float)
-    # flipped the (row,column) indices of indrefwaves_frac rebin (not sure if it works)
-    dwaves = (indsubwaves - np.resize(indrefwaves_frac,[nsubwave,nline]))*dispersion
-    # flipped the (row,column) indices of indrefwaves rebin
-    indsubwaves += np.resize(indrefwaves,[nsubwave,nline])
+#     fluxes = param[find]
+#     refwaves = param[wind]
+#     indrefwaves_real = (refwaves - wave[0]) / dispersion
+#     indrefwaves = (indrefwaves_real).astype(int)
+#     indrefwaves_frac = indrefwaves_real - indrefwaves.astype(float)
+#     # flipped the (row,column) indices of indrefwaves_frac rebin (not sure if it works)
+#     dwaves = (indsubwaves - np.resize(indrefwaves_frac,[nsubwave,nline]))*dispersion
+#     # flipped the (row,column) indices of indrefwaves rebin
+#     indsubwaves += np.resize(indrefwaves,[nsubwave,nline])
     
-    yvals = np.zeros(nwave)
-    for i in range (0,nline):
-        # flipped the (row,column) indices for indsubwaves
-        # Python cannot do a where(condition 1 and condition 2)  extraction like in IDL, 
-        # so I need to treat the limits separately and merge the indices for the 2 conditions
-        gind1 = np.where(indsubwaves[:,i] >= 0)[0]    
-        gind2 = np.where(indsubwaves[:,i] <= nwave-1)[0]
-        gind=list(set(gind1) & set(gind2))
-        count = len(gind)
-        # The "mask" parameter eliminates floating underflow by removing very large
-        # negative exponents. See http://www.idlcoyote.com/math_tips/underflow.html
-        # for more details.
-        if count > 0:
-            exparg = -np.power((dwaves[gind,i]/sigs[i]),2.)/2.    # flipped the (row,column) indices 
-            # edited the masking code
-            # mask = (abs(exparg) lt 80)
-            ms = np.where(abs(exparg) < 80)[0]
-            mask = np.zeros(len(exparg))
-            mask[ms] = np.ones(len(ms))
-            # flipped the (row,column) indices for indsubwaves
-            yvals[indsubwaves[gind,i]] = np.add(yvals[indsubwaves[gind,i]],np.transpose(fluxes[i]*mask*np.exp(exparg*mask)))
-            # yvals[indsubwaves[gind,i]] = np.transpose(fluxes[i]*mask*np.exp(exparg*mask))
+#     yvals = np.zeros(nwave)
+#     for i in range (0,nline):
+#         # flipped the (row,column) indices for indsubwaves
+#         # Python cannot do a where(condition 1 and condition 2)  extraction like in IDL, 
+#         # so I need to treat the limits separately and merge the indices for the 2 conditions
+#         gind1 = np.where(indsubwaves[:,i] >= 0)[0]    
+#         gind2 = np.where(indsubwaves[:,i] <= nwave-1)[0]
+#         gind=list(set(gind1) & set(gind2))
+#         count = len(gind)
+#         # The "mask" parameter eliminates floating underflow by removing very large
+#         # negative exponents. See http://www.idlcoyote.com/math_tips/underflow.html
+#         # for more details.
+#         if count > 0:
+#             exparg = -np.power((dwaves[gind,i]/sigs[i]),2.)/2.    # flipped the (row,column) indices 
+#             # edited the masking code
+#             # mask = (abs(exparg) lt 80)
+#             ms = np.where(abs(exparg) < 80)[0]
+#             mask = np.zeros(len(exparg))
+#             mask[ms] = np.ones(len(ms))
+#             # flipped the (row,column) indices for indsubwaves
+#             yvals[indsubwaves[gind,i]] = np.add(yvals[indsubwaves[gind,i]],np.transpose(fluxes[i]*mask*np.exp(exparg*mask)))
+#             # yvals[indsubwaves[gind,i]] = np.transpose(fluxes[i]*mask*np.exp(exparg*mask))
 
-    return yvals
+#     return yvals
 

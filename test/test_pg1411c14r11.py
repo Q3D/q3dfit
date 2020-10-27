8bd77b3210bff0823ac692af279fe4905e113eee
David Rupke <drupke@gmail.com>
2020-10-27 16:43:04 -0500

    Updated call to MAKEQSOTEMPLATE in PG1411 test. Added a couple variables to variables.txt; end-of-line fixes to FITSPEC from Spyder.

--- test/test_pg1411c14r11.py
+++ test/test_pg1411c14r11.py
@@ -2,7 +2,7 @@ from q3dfit.common import q3df,makeqsotemplate
 
 volume = '/Volumes/fingolfin/ifs/gmos/cubes/pg1411/'
 outpy = volume + 'pg1411qsotemplate.npy'
-infits = volume + 'pg1411rb3.fits'
+infits = volume + 'pg1411rb1.fits'
 
 makeqsotemplate.makeqsotemplate(infits,outpy,dataext=None,dqext=None,waveext=None)
 q3df.q3df('pg1411',cols=[14],rows=[11])

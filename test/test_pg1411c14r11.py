cbff1a292cfe807d51a8f0c8d4085cbf302079cf
David Rupke <drupke@gmail.com>
2020-11-09 17:16:00 -0600

    Added q3da call to PG1411 test fcn.

--- test/test_pg1411c14r11.py
+++ test/test_pg1411c14r11.py
@@ -1,4 +1,4 @@
-from q3dfit.common import q3df,makeqsotemplate
+from q3dfit.common import q3df,q3da,makeqsotemplate
 
 volume = '/Volumes/fingolfin/ifs/gmos/cubes/pg1411/'
 outpy = volume + 'pg1411qsotemplate.npy'
@@ -6,3 +6,4 @@ infits = volume + 'pg1411rb1.fits'
 
 makeqsotemplate.makeqsotemplate(infits,outpy,dataext=None,dqext=None,waveext=None)
 q3df.q3df('pg1411',cols=[14],rows=[11])
+q3da.q3da('pg1411',cols=[14],rows=[11])

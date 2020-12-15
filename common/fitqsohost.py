a1d362d6c48eb0a9852e194a7435b47c15b68097
Andrey Vayner <Endeavour@Andreys-MacBook-Pro.local>
2020-12-15 17:52:49 -0500

    adding ppxf functionality for stellar continuum fitting to fitqsohost.py. Minor fixes to fitqsohost() after testing blrpar with additional broad emission line fitting when removing the quasar emission.

--- common/fitqsohost.py
+++ common/fitqsohost.py
@@ -1,6 +1,10 @@
 import numpy 
 from q3dfit.common import qsohostfcn
 import lmfit
+import ppxf as ppxf_package
+from ppxf.ppxf import ppxf
+import ppxf.ppxf_util as util
+from time import perf_counter as clock
 
 def qsotemplate_model_exponential(wave,qso_model,a,b,c,d,e,f,g,h):
     
@@ -222,24 +226,24 @@ def fitqsohost(wave,flux,weight,template_wave,template_flux,index,ct_coeff=None,
     if blrpar:
         counter = 0
         for i in numpy.arange(0,len(blrpar)/3.):
-            gaussian_name = 'g_' + str(blrpar[counter+1])
+            gaussian_name = 'g_' + str(int(blrpar[counter+1]))
             gaussian_model = lmfit.models.GaussianModel(prefix=gaussian_name)
             gaussian_model_parameters = gaussian_model.make_params()
             gaussian_model_parameters[gaussian_name+'amplitude'].set(value=blrpar[counter])
-            gaussian_model_parameters[gaussian_name+'center'].set(value=blrpar[counter+1])
-            gaussian_model_parameters[gaussian_name+'sigma'].set(value=blrpar[counter+2],min=10,max=400)
+            gaussian_model_parameters[gaussian_name+'center'].set(value=blrpar[counter+1],min=blrpar[counter+1]-blrpar[counter+1]*0.005,max=blrpar[counter+1]+blrpar[counter+1]*0.005)
+            gaussian_model_parameters[gaussian_name+'sigma'].set(value=blrpar[counter+2],min=10,max=100)
 
             ymod += gaussian_model
-            params = gaussian_model_parameters
+            params += gaussian_model_parameters
             counter += 3
 
 
 
 
-    result = ymod.fit(iflux,params,qso_model=qsoflux[index],wave=iwave)#,x=x_to_fit) #
+    result = ymod.fit(iflux,params,qso_model=qsoflux[index],wave=iwave,x=iwave)#,x=x_to_fit) #
 
     comps = result.eval_components(wave=wave,qso_model=qsoflux,x=wave)
-    y_final = result.eval(wave=wave,qso_model=qsoflux)
+    y_final = result.eval(wave=wave,qso_model=qsoflux,x=wave)
 
 
     if 'fcn_test' in kwargs.keys():
@@ -249,3 +253,89 @@ def fitqsohost(wave,flux,weight,template_wave,template_flux,index,ct_coeff=None,
     else:
         return y_final
 
+
+
+def fit_cont_ppxf(wave,flux,z_stars,index,template):
+    
+    c = 299792.458
+    z = z_stars
+    mask = ((wave > 3540) & (wave < 7450))
+    flux_to_fit = flux[index.astype(int)]
+    galaxy = flux_to_fit/numpy.median(flux_to_fit)
+    lam_gal = wave[index.astype(int)]
+    loglam_gal = numpy.log10(wave[index.astype(int)])
+    noise = numpy.full_like(galaxy,numpy.std(galaxy))
+
+
+    #Setting instrument properties:
+    frac = wave[1]/wave[0]    # Constant lambda fraction per pixel
+    dlam_gal = (frac - 1)*wave[index.astype(int)]   # Size of every pixel in Angstrom
+    wdisp = numpy.full_like(galaxy,1)        # Intrinsic dispersion of every pixel, in pixels units
+    fwhm_gal = 2.355*wdisp*dlam_gal # Resolution FWHM of every pixel, in Angstroms
+    velscale = numpy.log(frac)*c    # Velocity scale in km/s per pixel (eq.8 of Cappellari 2017)
+
+
+    #Setting up models to fit:
+
+    if template == 'Vazdekis':
+        ppxf_dir = path.dirname(path.realpath(ppxf_package.__file__))
+        # Read the list of filenames from the Single Stellar Population library
+        # by Vazdekis (2010, MNRAS, 404, 1639) http://miles.iac.es/. A subset
+        # of the library is included for this example with permission
+        vazdekis = glob.glob(ppxf_dir + '/miles_models/Mun1.30Z*.fits')
+        fwhm_tem = 2.51 # Vazdekis+10 spectra have a constant resolution FWHM of 2.51A.
+    
+        # Extract the wavelength range and logarithmically rebin one spectrum
+        # to the same velocity scale of the galaxy spectrum, to determine
+        # the size needed for the array which will contain the template spectra.
+        #
+        hdu = fits.open(vazdekis[0])
+        ssp = hdu[0].data
+        h2 = hdu[0].header
+        lam_temp = h2['CRVAL1'] + h2['CDELT1']*numpy.arange(h2['NAXIS1'])
+        lamRange_temp = [numpy.min(lam_temp), numpy.max(lam_temp)]
+        sspNew = util.log_rebin(lamRange_temp, ssp, velscale=velscale)[0]
+        templates = numpy.empty((sspNew.size, len(vazdekis)))
+
+        #Interpolates the galaxy spectral resolution at the location of every pixel
+        #of the templates. Outside the range of the galaxy spectrum the resolution
+        # will be extrapolated, but this is irrelevant as those pixels cannot be
+        # used in the fit anyway.
+        fwhm_gal = numpy.interp(lam_temp, lam_gal, fwhm_gal)
+
+        fwhm_dif = numpy.sqrt((fwhm_gal**2 - fwhm_tem**2).clip(0))
+        sigma = fwhm_dif/2.355/h2['CDELT1'] # Sigma difference in pixels
+
+        for j, fname in enumerate(vazdekis):
+            hdu = fits.open(fname)
+            ssp = hdu[0].data
+            ssp = util.gaussian_filter1d(ssp, sigma)  # perform convolution with variable sigma
+            sspNew = util.log_rebin(lamRange_temp, ssp, velscale=velscale)[0]
+            templates[:, j] = sspNew/numpy.median(sspNew) # Normalizes templates
+
+    if template == 'PG1411':
+        fwhm_tem = 1.35 #1A?
+        template_PG1411 = numpy.load('pg1411hosttemplate.npy',allow_pickle=True).item()
+        lam_temp = template_PG1411['lambda']
+        lamRange_temp = [numpy.min(lam_temp), numpy.max(lam_temp)]
+        ssp_PG1411 = template_PG1411['flux']
+        sspNew_PG1411 = util.log_rebin(lamRange_temp, ssp_PG1411, velscale=velscale)[0]
+        sspNew_PG1411 = sspNew_PG1411/numpy.median(sspNew_PG1411)
+        templates = sspNew_PG1411
+
+    dv = c*numpy.log(lam_temp[0]/lam_gal[0])    # eq.(8) of Cappellari (2017)
+    goodpixels = util.determine_goodpixels(numpy.log(lam_gal), lamRange_temp, z=z+0.001)
+
+    vel = c*numpy.log(1 + z)   # eq.(8) of Cappellari (2017)
+    start = [vel, 200.]  # (km/s), starting guess for [V, sigma]
+    t = clock()
+
+    pp = ppxf(templates, galaxy, noise, velscale, start,
+            goodpixels=goodpixels, plot=True, moments=4,
+              degree=12, vsyst=dv, clean=False, lam=lam_gal)
+
+
+    print("Formal errors:")
+    print("     dV    dsigma   dh3      dh4")
+    print("".join("%8.2g" % f for f in pp.error*numpy.sqrt(pp.chi2)))
+    print('Elapsed time in PPXF: %.2f s' % (clock() - t))

08aad5b9fc5964ed56600fa72232665b41ca4673
Nadia Zakamska <35604744+zakamska@users.noreply.github.com>
2021-06-01 22:51:37 -0400

    Handling new data tables, vac-to-air warning, updated history

--- common/linelist.py
+++ common/linelist.py
@@ -1,7 +1,7 @@
 def linelist(inlines=None,linelab=True,waveunit='Angstrom',vacuum=True):
     """
-    Generates an astropy Table of lines from the master files linelist_air.tbl
-    and linelist_vac.tbl
+    Generates an astropy Table of lines from the master files provided in
+    /data/linelists
     
     Returns:
     
@@ -45,12 +45,13 @@ def linelist(inlines=None,linelab=True,waveunit='Angstrom',vacuum=True):
         
         3.
         
-        mylist=['Paa', 'Halpha', 'junk']
+        mylist=['Paa', 'Halpha', 'junk', 'H2_00_S0']
         u=linelist(inlines=mylist,vacuum=False,waveunit='micron',linelab=False)
         
-        will produce a table of air wavelengths in micron for two lines, report
-        that one line is not in the line list, convert to air wavelengths and 
-        to microns and remove the line lables
+        will produce a table of air wavelengths in micron for three lines, report
+        that line 'junk' is not in the line list, convert to air wavelengths and 
+        to microns, complain that H2_00_S0 is outside the validity of the Morton's
+        formula for vacuum to air conversion, and remove the line lables
         
         4.
         
@@ -61,44 +62,47 @@ def linelist(inlines=None,linelab=True,waveunit='Angstrom',vacuum=True):
         OR
         
         u=linelist()
-        wv=np.array(u['lines'][(u['name']=='Halpha')])      
+        wv=np.array(u['lines'][(u['name']=='Halpha')])
+        
+    History:
+    2020jun24 Created by Nadia Zakamska to work with two tables, one in air, one
+        in vacuum, bringing them on the user-specified wavelength scale
+    2021may31 Updated by NLZ to include new line tables, print warning if Morton
+        conversion from vacuum to air is not applicable    
         
     """
     import pdb
     from astropy.table import Table, vstack
-    import os
-    #https://stackoverflow.com/questions/4060221/how-to-reliably-open-a-file-in-the-same-directory-as-a-python-script
-
-    # Ideally the whole line file would be one list on vacuum scale, in which case
-    # we can simply do:
-    # all_lines=Table.read(os.path.dirname(__file__)+'/linelist_master.tbl',format='ipac')
-    # outlines=all_lines
     
-    # However, right now I have two different files.
-    lines_air=Table.read(os.path.dirname(__file__)+'/linelist_air.tbl',format='ipac')
-    lines_vac=Table.read(os.path.dirname(__file__)+'/linelist_vac.tbl',format='ipac')
-    # get everything on the same wavelength scale, air or vacuum depending on the
-    # user input; this must be done before any Angstrom to micron conversion
-    # is attempted
-    if ((vacuum!=True) & (vacuum!=False)):
-        print ('Incorrect input for vacuum vs air, proceeding with default (vacuum)')
-        vacuum=True
-    if (vacuum==False):
-        # meaning air is desired; converting from vacuum to air and stacking the 
-        # two tables into one output
-        temp=1.e4/lines_vac['lines']
-        lines_vac['lines']=lines_vac['lines']/(1.+6.4328e-5+
-                2.94981e-2/(146.-temp**2)+2.5540e-4/(41.-temp**2))
-        all_lines=vstack([lines_vac,lines_air])
-    if (vacuum==True):
-        # meaning vacuum is desired; converting from air to vacuum and stacking the
-        # two tables into one output
-        temp=1.e4/lines_air['lines']
-        lines_air['lines']=lines_air['lines']*(1.+6.4328e-5+
-                2.94981e-2/(146.-temp**2)+2.5540e-4/(41.-temp**2))
-        all_lines=vstack([lines_vac,lines_air])
+    # I will have more files here, and hopefully the script will be able to
+    # handle them through this list, but the default is they are all in vacuum
+    lines_DSNR=Table.read('../data/linelists/linelist_DSNR.tbl',format='ipac')
+    lines_H2=Table.read('../data/linelists/linelist_H2.tbl',format='ipac')
+    all_tables=[lines_DSNR,lines_H2]
+    all_units=[lines_DSNR['lines'].unit,lines_H2['lines'].unit]
+
+    # get everything on the user-requested units:
+    if ((waveunit!='Angstrom') & (waveunit!='micron')):
+        print ('Wave unit ',waveunit,' not recognized, returning Angstroms')
+    if (waveunit=='micron'):
+        for i,u in enumerate(all_units):
+            if (u=='Angstrom'): 
+                all_tables[i]['lines']=all_tables[i]['lines']*1.e-4
+                all_tables[i]['lines'].unit='micron'
+        # now everything is on the same units, let's stack all the tables:
+        all_lines=vstack(all_tables)
+        all_lines['lines'].unit='micron'
+    if (waveunit=='Angstrom'):
+        for i,u in enumerate(all_units):
+            if (u=='micron'): 
+                all_tables[i]['lines']=all_tables[i]['lines']*1.e4
+                all_tables[i]['lines'].unit='Angstrom'
+        # now everything is on the same units, let's stack all the tables:
+        all_lines=vstack(all_tables)
+        all_lines['lines'].unit='Angstrom'
+    # and this will be my output variable for the user
     outlines=all_lines
-   
+        
     # a helper function that removes duplicate lines to check for duplicates
     # https://stackoverflow.com/questions/12897374/get-unique-values-from-a-list-in-python
     def uniqlist(inlist):
@@ -127,23 +131,36 @@ def linelist(inlines=None,linelab=True,waveunit='Angstrom',vacuum=True):
                 print ('Returning the first database occurrence')
                 sub_table=sub_table[0]
             outlines=vstack([outlines, sub_table])    
+        # the healthy outcome is that the number of entries in the outlines
+        # is the same as the number of entries in the inlines
+        if (len(inlines)!=len(outlines)):
+            print('Input list size different from output table size, most likely')
+            print('because some lines were not found in the database')
 
     # if the user doesn't want Latex labels, remove them
     if (linelab==False):
         outlines.keep_columns(['name','lines'])
         
-    # switch to microns if requested. Only Angstrom and microns are recognized 
-    if ((waveunit!='Angstrom') & (waveunit!='micron')):
-        print ('Wave unit ',waveunit,' not recognized, returning Angstroms')
-    if (waveunit=='micron'):
-        outlines['lines']=outlines['lines']*1.e-4
-        outlines['lines'].unit='micron'
-        
-    # the healthy outcome is that the number of entries in the outlines
-    # is the same as the number of entries in the inlines
-    if inlines: 
-        if (len(inlines)!=len(outlines)):
-            print('Input list size different from output table size, most likely')
-            print('because some lines were not found in the database')
+    if ((vacuum!=True) & (vacuum!=False)):
+        print ('Incorrect input for vacuum vs air, proceeding with default (vacuum)')
+        vacuum=True
+    if (vacuum==False):
+        # meaning air is desired; first let's check whether we need to issue
+        # a Morton warning:
+        if (outlines['lines'].unit=="Angstrom"):    
+            morton_fail=((outlines['lines']<2000) | (outlines['lines']>25000))
+            if (sum(morton_fail)>0): print('Line(s) ',outlines['name'][morton_fail],
+                                           ' outside of Morton validity for conversion to air')
+        if (outlines['lines'].unit=="micron"):    
+            morton_fail=((outlines['lines']<0.2) | (outlines['lines']>2.5))
+            if (sum(morton_fail)>0): print('Line(s) ',outlines['name'][morton_fail],
+                                           ' outside of Morton validity for conversion to air')
+
+        if (outlines['lines'].unit=="Angstrom"): 
+            temp=1.e4/outlines['lines']
+        else:
+            temp=1./outlines['lines']
+        outlines['lines']=outlines['lines']/(1.+6.4328e-5+
+                2.94981e-2/(146.-temp**2)+2.5540e-4/(41.-temp**2))
 
     return outlines

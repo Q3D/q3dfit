7ad61fae3601e5603e7d4b317580c6713f0d4b1f
David Rupke <drupke@gmail.com>
2021-06-16 13:44:01 -0500

    Working on fitting Makani SDSS spectrum

--- test/test_makanisdss.py
+++ test/test_makanisdss.py
@@ -1,5 +1,9 @@
+import numpy as np
 from q3dfit.common.q3df import q3df
-q3df('makanisdss', cols=1, rows=1, quiet=False)
-
 from q3dfit.common.q3da import q3da
-q3da('makanisdss', cols=1, rows=1, quiet=False)
+
+initproc = \
+    np.load('/Users/drupke/specfits/q3dfit/testing/makanisdss/initproc.npy',
+            allow_pickle=True)
+q3df(initproc[()], cols=1, oned=True)
+q3da(initproc[()], cols=1, oned=True)

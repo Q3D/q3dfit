Fixed issue with units in MIR emission line fitting. Changed default value for MIR emission line width to something reasonable.
The code now finds an appropriate width for one test MIR line. at least. Note: The MIR emission line fitting is currently done in angstrom, but questfit still works in micron.

common/fitspec.py
@@ -438,9 +438,6 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
                     fcncontfit(gdlambda, gdflux, gdweight, templatelambdaz_tmp,
                                templateflux_tmp, ct_indx, zstar,
                                quiet=quiet, **argscontfit_use)
-                if 'plotMIR' in initdat:    # Test plot here - need to transfer this to q3dfa later
-                  print('Plotting')
-                  plot_quest(gdlambda[ct_indx], gdflux[ct_indx], continuum[ct_indx], ct_coeff, initdat)
 
                 ppxf_sigma = 0.
                 if initdat['fcncontfit'] == 'ifsf_fitqsohost' and \
@@ -591,7 +588,6 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
                     fline = interp1d(gdlambda, gdflux_nocnt, kind='linear')
                     # Check that line wavelength is in data range
                     # Use first component as a proxy for all components
-                    if initdat['fcncontfit']=='questfit': listlinesz[line][0] *= 1e-4 # micron
                     if listlinesz[line][0] >= min(gdlambda) and \
                         listlinesz[line][0] <= max(gdlambda):
                         peakinit[line] = fline(listlinesz[line])
@@ -609,9 +605,6 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
             for line in initdat['lines']:
                 siginit_gas[line] = \
                     np.zeros(initdat['maxncomp']) + siginit_gas_def
-        if initdat['fcncontfit']=='questfit':
-          for el in siginit_gas.keys():   siginit_gas[el][0] *= 1e-4  # micron
-          siglim_gas *= 1e-4  # micron
 
         # Fill out parameter structure with initial guesses and constraints
         impModule = import_module('q3dfit.init.' + initdat['fcninitpar'])
@@ -635,6 +628,8 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
         if testsize == 0:
             raise Exception('Bad initial parameter guesses.')
 
+        if initdat['fcncontfit']=='questfit':   # go back to angstrom for emission line fitting
+            gdlambda *= 1e4
 
         efitModule = import_module('q3dfit.common.'+fcnlinefit)
         elin_lmfit = getattr(efitModule, 'run_'+fcnlinefit)
@@ -642,10 +637,14 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
             elin_lmfit(gdlambda, gdflux_nocnt, gdweight_nocnt, parinfo=parinit,
                        maxiter=1000, quiet=quiet)
 
+        if initdat['fcncontfit']=='questfit':   # go back to micron again
+            gdlambda *= 1e-4
+
         if 'plotMIR' in initdat.keys():    # Test plot here - need to transfer this to q3dfa later
           print('Plotting')
           from matplotlib import pyplot as plt
-          plot_quest(gdlambda, gdflux, continuum+specfit, ct_coeff, initdat, templ_mask=ct_indx, lines=[12.8], linespec=specfit)
+          plot_quest(gdlambda, gdflux, continuum+specfit, ct_coeff, initdat, lines=[12.8], linespec=specfit)
+          #plot_quest(gdlambda, gdflux, continuum, ct_coeff, initdat)
 
         param = parout
         covar = lmout.covar

common/questfit.py
@@ -1,6 +1,7 @@
 import numpy as np
 import lmfit
 import copy
+from scipy import constants
 from q3dfit.common import interptemp
 from q3dfit.common import questfitfcn
 from q3dfit.common import questfit_readcf
@@ -13,8 +14,8 @@ import q3dfit
 
 
 def questfit(wlambda, flux, weights, singletemplatelambda, singletemplateflux, index, 
-    z, quiet=True, config_file=None, global_ice_model='', global_ext_model='', \
-    models_dictionary={}, template_dictionary={}, fitran=None):
+    z, quiet=True, config_file=None, global_ice_model='None', global_ext_model='None', \
+    models_dictionary={}, template_dictionary={}, fitran=None, convert2Flambda=True):
     '''Function defined to fit the MIR continuum
 
     Parameters
@@ -61,7 +62,7 @@ def questfit(wlambda, flux, weights, singletemplatelambda, singletemplateflux, i
     if fitran:
         flux = flux[ np.logical_and(wlambda>=fitran[0]), np.logical_and(wlambda<=fitran[1]) ]
         wlambda = wlambda[ np.logical_and(wlambda>=fitran[0]), np.logical_and(wlambda<=fitran[1]) ]
-    if len(global_ext_model)>0:     global_extinction = True
+    if global_ext_model != 'None':     global_extinction = True
 
 
     if singletemplatelambda!=b'0':
@@ -224,17 +225,36 @@ def questfit(wlambda, flux, weights, singletemplatelambda, singletemplateflux, i
             models_dictionary[i] = temp_value_rebin/temp_value_rebin.max()
 
         models_dictionary['wave'] = wlambda/(1+z)
-
         
+        if convert2Flambda:
+            #import pdb; pdb.set_trace()
+            flux *= constants.c/(wlambda*1e-9)**2 *1e-23
+            for el in models_dictionary.keys():
+                if not (global_ext_model in el) and not (global_ice_model in el) and not ('ext' in el) and not ('ice' in el):
+                    models_dictionary[el]*= constants.c/(wlambda*1e-9)**2 *1e-23
+
         flux_cut = flux[index]
         models_dictionary_cut = copy.deepcopy(models_dictionary)
         for el in models_dictionary.keys():
             models_dictionary_cut[el] = models_dictionary_cut[el][index]
 
+
         result = model.fit(flux_cut,param,**models_dictionary_cut,max_nfev=int(1e5),method='least_squares',nan_policy='omit')#method='least_squares'nan_policy='omit'
 
-        best_fit = result.eval(**models_dictionary)
-        comp_best_fit = result.eval_components()
+        best_fit = result.eval(**models_dictionary) # use models_dictionary rather than models_dictionary_cut to evaluate over all wavelengths within fitran (not just [index])
+        comp_best_fit = result.eval_components(**models_dictionary)
+
+        if convert2Flambda:
+            flux /= (constants.c/(wlambda*1e-9)**2 *1e-23)
+            best_fit /= (constants.c/(wlambda*1e-9)**2 *1e-23)
+            for el in comp_best_fit.keys():
+                if not (global_ext_model in el) and not (global_ice_model in el) and not ('ext' in el) and not ('ice' in el):
+                    try:
+                        comp_best_fit[el] /= (constants.c/(wlambda*1e-9)**2 *1e-23)
+                    except Exception as e:
+                        print(e)
+                        import pdb; pdb.set_trace()
+
         ct_coeff = {'MIRparams': result.params, 'comp_best_fit': comp_best_fit}
 
         #return best_fit,comp_best_fit,result

init/f21219mir.py
@@ -63,7 +63,7 @@ def f21219mir():
         linetie[i] = '[NeII]128130'
         ncomp[i] = np.full((ncols,nrows),maxncomp)
         zinit_gas[i] = np.full((ncols,nrows,maxncomp),0.)
-        siginit_gas[i] = np.full(maxncomp,50.)
+        siginit_gas[i] = np.full(maxncomp,1000.)
         zinit_stars=np.full((ncols,nrows),0.0)
 
 
@@ -93,7 +93,7 @@ def f21219mir():
 
     # Velocity dispersion limits and fixed values
     siglim_gas = np.ndarray(2)
-    siglim_gas[:] = [5, 1500]
+    siglim_gas[:] = [5, 4000]
 
     #
     # Output structure
@@ -132,7 +132,7 @@ def f21219mir():
             'logfile': logfile,
             'siglim_gas': siglim_gas,
             'siginit_gas': siginit_gas,
-            'siginit_stars': 50,
+            'siginit_stars': 100,
             'nocvdf': 1,
             'waveext': 4,
             'datext': 1,
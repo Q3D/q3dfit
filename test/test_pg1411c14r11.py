e94ba76cd3174291497e17c8b12f9424cb7a099e
Anna Murphree <annammurphree@gmail.com>
2021-06-21 14:13:18 -0500

    Update test_pg1411c14r11.py

--- test/test_pg1411c14r11.py
+++ test/test_pg1411c14r11.py
@@ -6,13 +6,22 @@
 # infits = volume + 'pg1411rb1.fits'
 # makeqsotemplate(infits, outpy, dataext=None, dqext=None, waveext=None)
 
-from q3dfit.common.q3df import q3df
-q3df('pg1411', cols=14, rows=11, quiet=False)
+#from q3dfit.common.q3df import q3df
+#q3df('pg1411', cols=14, rows=11, quiet=False)
+
+import os
+cwd = os.getcwd()
+print("Current working directory: {0}".format(cwd))
 
 from q3dfit.common.q3da import q3da
 q3da('pg1411', cols=14, rows=11, quiet=False)
+#%% To test a simulated PG1411 spaxel with continuum fitting by questfit:
+import os
+cwd = os.getcwd()
+print("Current working directory: {0}".format(cwd))
 
-# Test creation of Gonzalez-Delgado templates
-# from q3dfit.common.gdtemp import gdtemp
-# gdtemp('/Users/drupke/Documents/stellar_models/gonzalezdelgado/SSPGeneva.z020',
-#        '/Users/drupke/Documents/stellar_models/gonzalezdelgado/SSPGeneva_z020.npy')
+#from q3dfit.common.q3df import q3df
+#q3df('pg1411_and_Spitzer', cols=0, rows=0, quiet=False)
+
+from q3dfit.common.q3da import q3da
+q3da('pg1411_and_Spitzer', cols=1, rows=1, quiet=False)

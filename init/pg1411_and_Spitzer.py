fe2e57bacd1a13123dbcbda3ec415b7fc59f41fa
Anna Murphree <annammurphree@gmail.com>
2021-06-21 14:13:16 -0500

    Update pg1411_and_Spitzer.py

--- init/pg1411_and_Spitzer.py
+++ init/pg1411_and_Spitzer.py
@@ -28,36 +28,29 @@ def pg1411_and_Spitzer():
     fitrange = np.array([5.422479152679443,29.980998992919922])*10000  # angstrom
     
     #   These are unique to the user
-    # volume = '/Users/dwylezal/EmmyNoether_Science/Q3D/JWST_ERS_Planning/Software/PG1411/'
-    volume = '/Users/caroline/Documents/ARI-Heidelberg/Q3D/PG1411/pg1411/'
-    #volume = '/Users/Endeavour/Projects/Q3D_dev/pyfsfit'
-    test_cube = '../test/test_questfit/IRAS21219m1757_dlw_qst_mock_cube.fits'
-    infile = test_cube #volume+gal+outstr+'.fits'
-    #mapdir = volume+gal+'/'+outstr+'/'
-    #outdir = volume+gal+'/'+outstr+'/'
-    mapdir = '../test/test_questfit/'
-    outdir = mapdir
+    volume = '/Users/annamurphree/Docs/Rupke Research/q3d/pg1411/'
+    test_cube = '/Users/annamurphree/Docs/Rupke Research/q3d/q3dfit_q3da_questfit_plots/test/test_questfit/IRAS21219m1757_dlw_qst_mock_cube.fits'
+    infile = test_cube 
+    mapdir = volume+'outdir/'+outstr+'/'
+    outdir = volume+'outdir/'+outstr+'/'
     qsotemplate = volume+gal+'qsotemplate.npy'
-    stellartemplates =  \
-        volume+gal+'hosttemplate.npy'
-    logfile = outdir+gal+'_fitlog.txt'
-    #batchfile = '/Users/dwylezal/ESO_Fellowship/JWST_ERS_Planning/Software/ifsfit-master/common/fitloop.pro'
-    #batchdir = '/Users/dwylezal/ESO_Fellowship/JWST_ERS_Planning/Software/'
-    batchfile = '/Users/caroline/Documents/ARI-Heidelberg/Q3D/IFSFIT/ifsfit/common/ifsf_fitloop.pro' 
-    batchdir = '/Users/caroline/Documents/ARI-Heidelberg/Q3D/IFSFIT/'
+    stellartemplates = volume+gal+'hosttemplate.npy'
+    logfile = volume+gal+'_fitlog.txt'
+    batchfile = '/Users/annamurphree/Docs/Rupke Research/q3d/q3dfit_q3da_questfit_plots/common/fitloop.py'
+    batchdir = '/Users/annamurphree/Docs/Rupke Research/q3d/q3dfit/'
 
     
     ### for our test object, pg1411, nothing needs to be changed here for now, make more flexible later
 
     
     ### more MIR settings
-    #   These are unique to the user
+    #  These are unique to the user
     #  Include Spitzer source (independently of PG1411 for now for testing purposes)
     global_extinction = True
     global_ice_model = 'ice_hc'
     global_ext_model = 'CHIAR06'
-    directory = '../test/test_questfit/'
-    cffilename = '../test/test_questfit/IRAS21219m1757_dlw_qst.cf'
+    directory = '/Users/annamurphree/Docs/Rupke Research/q3d/q3dfit_q3da_questfit_plots/test/test_questfit/'
+    cffilename = '/Users/annamurphree/Docs/Rupke Research/q3d/q3dfit_q3da_questfit_plots/test/test_questfit/IRAS21219m1757_dlw_qst.cf'
     config_file = questfit_readcf.readcf(cffilename)
     #MIRz=0.112    
 
@@ -166,11 +159,20 @@ def pg1411_and_Spitzer():
                             'global_ext_model':global_ext_model,
                             'models_dictionary':{},
                             'template_dictionary':{}},
+            # in plot_spec: x/ystyle = log or lin (plots it linearly), 
+            #               xunit = micron or Angstrom,
+            #               yunit = flambda, lambdaflambda (= nufnu), or fnu
+            #               mode = light or dark
+            'argscontplot': {'xstyle':'lin',
+                             'ystyle':'lin',
+                             'xunit':'micron',
+                             'yunit':'flambda',
+                             'mode':'dark'},
             'argslinelist': {'vacuum': False},
             'startempfile': stellartemplates,
             'argspltlin1': argspltlin1,
             # 'donad': 1,
-            'decompose_qso_fit': 1,
+            #'decompose_qso_fit': 1,
             # 'remove_scattered': 1,
             'fcncheckcomp': 'checkcomp',
             'fcncontfit': 'questfit',
@@ -197,6 +199,9 @@ def pg1411_and_Spitzer():
             'dqext': 3,
             'zerodq': True,
             'plotMIR': True,
+            # in plot_spec: plotstyle = 1 plots log(continuum), plotstyle = 2 plots it linearly
+            'plotstyle': 1,
+            'qsotempfile': qsotemplate,
         }
 
     return(init)

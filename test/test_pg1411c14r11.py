6fdef097921b8bec504dad4c35d4f47fdb1a7882
Anna Murphree <annammurphree@gmail.com>
2021-06-21 17:48:30 -0500

    Update test_pg1411c14r11.py

--- test/test_pg1411c14r11.py
+++ test/test_pg1411c14r11.py
@@ -5,7 +5,7 @@
 # infits = volume + 'pg1411rb1.fits'
 # makeqsotemplate(infits, outpy, dataext=None, dqext=None, waveext=None)
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #from q3dfit.common.q3df import q3df
 #q3df('pg1411', cols=14, rows=11, quiet=False)
 
@@ -22,17 +22,17 @@ print("Current working directory: {0}".format(cwd))
 
 #from q3dfit.common.q3df import q3df
 #q3df('pg1411_and_Spitzer', cols=0, rows=0, quiet=False)
-=======
+#=======
 import numpy as np
-import pdb
+#import pdb
 from q3dfit.common.q3df import q3df
 from q3dfit.common.q3da import q3da
 
-initproc = np.load('/Users/drupke/specfits/gmos/pg1411/rb3/initproc.npy', 
+initproc = np.load('/Users/annamurphree/Docs/Rupke Research/q3d/pg1411/initproc.npy', 
                    allow_pickle=True)
-q3df(initproc[()], cols=14, rows=11, quiet=False)
-q3da(initproc[()], cols=14, rows=11, quiet=False)
->>>>>>> 11c09e4c1011a065371c0899150a2d2ba3aa7c72
 
-from q3dfit.common.q3da import q3da
-q3da('pg1411_and_Spitzer', cols=1, rows=1, quiet=False)
+q3df(initproc[()], cols=14, rows=11, quiet=False)
+q3da(initproc[()], cols=14, rows=11, quiet=False)\
+
+#from q3dfit.common.q3da import q3da
+#q3da('pg1411_and_Spitzer', cols=1, rows=1, quiet=False)

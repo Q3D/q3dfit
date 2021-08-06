3994429fa5726d1ec13819d396aa38d9be215d98
David Rupke <drupke@gmail.com>
2021-08-06 11:07:16 -0500

    Fixing broken merge at commit 6ccb by adding change to run_q3dfit.ipynb from commit e0d2. Also updating run_q3dfit.ipynb for instructions to scriptable call to q3df and resetting outputs / execution counts.

--- test/test_pg1411c14r11.py
+++ test/test_pg1411c14r11.py
@@ -5,15 +5,12 @@
 # infits = volume + 'pg1411rb1.fits'
 # makeqsotemplate(infits, outpy, dataext=None, dqext=None, waveext=None)
 
-import sys
-sys.path.append('/Users/annamurphree/Docs/Rupke Research/q3dfit')
-
 import numpy as np
 from q3dfit.common.q3df import q3df
 from q3dfit.common.q3da import q3da
 
 # Anna:
-#initproc = np.load('/Users/annamurphree/Docs/Rupke Research/q3d/pg1411/initproc.npy',allow_pickle=True)
+# initproc = np.load('/Users/annamurphree/Docs/Rupke Research/q3d/pg1411/initproc.npy',
 # Dave:
 #initproc = np.load('/Users/drupke/specfits/gmos/pg1411/rb3/initproc.npy',
                    #allow_pickle=True)

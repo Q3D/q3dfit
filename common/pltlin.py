Bugfix when ncomp differs from one line group to another; and for emission line plot center wavelength computation.

common/pltlin.py
@@ -124,7 +124,8 @@ def pltlin(instr, pltpar, outfile):
                     linwav[i] = instr['param'][f'{lmline}_0_cwv']
                 # otherwise
                 else:
-                    linwav[i] = instr['linelist']['lines'][i] * \
+                    idx = np.where(instr['linelist']['name'] == sub_linlab[i])
+                    linwav[i] = instr['linelist']['lines'][idx] * \
                         (1. + instr['zstar'])
             else:
                 linwav[i] = 0.
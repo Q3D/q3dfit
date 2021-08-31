- bug: switch to correct error (rather than variance!) caused PG1411 test to fail. Tracked down to the 'x_scale': 'jac' option passed to scipy.optimize.least_squares, which had been added to fix a convergence failure in some previous test. With this removed, PG1411, Makani, and Spitzer tests all pass. - added: 'arglinefit' dict in init file now can pass options (like 'x_scale': 'jac') to scipy optimizer, through lmfit. - change: if 'quiet' is set to False, scipy optimizer now prints status at each iteration in emission-line fit. - bug: depending on order in which doublet lines are listed, ensure that automatic bounds are in ascending order

common/fitspec.py
@@ -232,7 +232,6 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
     # Set up error in zstar
     zstar_err = 0.
 
-
 # ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 # # Pick out regions to fit
 # ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -599,18 +598,30 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
             # raise Exception('Bad initial parameter guesses.')
 
         # Actual fit
+        # from matplotlib import pyplot as plt
         # plt, ax = plt.subplots()
-        # ax.plot(gdlambda, gdflux_nocnt)
+        # ax.plot(gdlambda, 1./np.sqrt(gdweight_nocnt))
+        # ax.plot(gdlambda, gdflux__nocnt)
         # plt.show()
         # pdb.set_trace()
 
         # Actual fit
+        if quiet:
+            lmverbose = 0  # verbosity for scipy.optimize.least_squares
+        else:
+            lmverbose = 2
+        fit_kws = {'verbose': lmverbose}
         # x_scale = 'jac' is option to minimizer 'least_squares';
         # greatly speeds up multi-gaussian fit in at least one test case
         # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html
+        # can add here using 'argslinefit' dict in init file
+        if 'argslinefit' in initdat:
+            for key, val in initdat['argslinefit'].items():
+                fit_kws[key] = val
         lmout = emlmod.fit(gdflux_nocnt, fit_params, x=gdlambda,
                            method='least_squares', weights=gdweight_nocnt,
-                           nan_policy='omit', fit_kws={'x_scale':'jac'})
+                           nan_policy='omit', fit_kws=fit_kws)
+
         specfit = lmout.best_fit
         if not quiet:
             print(lmout.fit_report(show_correl=False))

init/parinit.py
@@ -173,8 +173,8 @@ def parinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp,
                     elif line1 in doublets['line2']:
                         iline1 = np.where(doublets['line2'] == line1)
                         if doublets['line1'][iline1] == line2:
-                            lower = 1. / doublets['lower'][iline1][0]
-                        fit_params[lmrat].min = lower
+                            upper = 1. / doublets['lower'][iline1][0]
+                        fit_params[lmrat].max = upper
                     # apply upper limit?
                     if 'upper' in lineratio.colnames:
                         upper = lineratio['upper'][ilinrat]
@@ -187,8 +187,8 @@ def parinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp,
                     elif line1 in doublets['line2']:
                         iline1 = np.where(doublets['line2'] == line1)
                         if doublets['line1'][iline1] == line2:
-                            upper = 1. / doublets['upper'][iline1][0]
-                        fit_params[lmrat].max = upper
+                            lower = 1. / doublets['upper'][iline1][0]
+                        fit_params[lmrat].min = lower
 
     # pass siglim_gas back because the default is set here, and it's needed
     # downstream
525cffdfda5518116f6c61354f1e21291af0ffcc
andreyvayner <avayner@ucsd.edu>
2021-09-17 16:52:09 -0400

    Adding a description for for the questfit .cf file in /docs. Updated similar information in common/questfit.readcf.py

--- common/questfit_readcf.py
+++ common/questfit_readcf.py
@@ -25,7 +25,7 @@ def readcf(filename):
         if i[0] == 'template' or i[0] == 'powerlaw' or i[0] == 'blackbody':
             #populate initilization dictionary with
 
-            #col 0: filename (if nessesary; path hardcoded in readcf.pro)
+            #col 0: filename (if nessesary; path hardcoded)
             #col 1: lower wavelength limit or normalization factor
             #col 2: upper wavelength limit or fix/free parameter (0 or 1) for normalization
             #col 3: name of ext. curve or ice feature
@@ -33,7 +33,10 @@ def readcf(filename):
             #col 5: fix/free parameter (0/1) for Av
             #col 6: S,M = screen or mixed extinction
             #col 7: initial guess for BB temperature or powerlaw index
-            #col 8: fix/free parameter (1/0) for BB temperature or powerlaw index
+            #col 8: fix/free parameter (0/1) for BB temperature or powerlaw index
+            #col 9: ice name model
+            #col 10: intial guess for ice absorption tau
+            #col 11: fix/free parameter (0/1) for tau
             init_questfit[i[0]+'_'+i[1]+'_'+i[4]+'_'+i[10]] = i[1:]
 
         if i[0] == 'absorption' or i[0] == 'extinction':

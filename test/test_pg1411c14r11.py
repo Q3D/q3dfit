9ce849c7a04d62b548f0299e9c07a11644d09dec
David Rupke <drupke@gmail.com>
2022-03-16 11:07:13 -0500

    doc/bugfix in run_q3dfit; obsolete file removed

--- test/test_pg1411c14r11.py
+++ test/test_pg1411c14r11.py
@@ -13,11 +13,7 @@ from q3dfit.common.q3df import q3df
 from q3dfit.common.q3da import q3da
 
 # Dave:
-initproc = np.load('/Users/drupke/specfits/gmos/pg1411/rb3/initproc.npy',
-                   allow_pickle=True)
-# Hadley:
-# initproc = np.load('/Users/hadley/Desktop/research/q3dfit/jnb/initproc.npy',
-#                    allow_pickle = "True")
+q3di = '/Users/drupke/specfits/gmos/pg1411/rb3/initproc.npy'
 
-q3df(initproc[()], cols=14, rows=11) #, quiet=False)
-q3da(initproc[()], cols=14, rows=11) #, quiet=False)
+q3df(q3di, cols=14, rows=11) #, quiet=False)
+q3da(q3di, cols=14, rows=11) #, quiet=False)

0304dd467176f8fcb1f2329773fab9e87b1de3dd
Yuzo Ishikawa <yuyuzo12@yuzos-mbp.acasmart.jh.edu>
2022-09-20 16:16:39 -0400

    bug fixes to cube-converter

--- q3dfit/nonJWST_datacube_converter.py
+++ q3dfit/nonJWST_datacube_converter.py
@@ -10,30 +10,27 @@ def datacube_convert(infile,CLOBBER=True):
     fdIN = fits.open(infile)
     enames = [dd.name.upper() for dd in fdIN]
     extens = {ext:(ext in enames) for ext in {'VAR':None,'DQ':None}}
+    sciEXT = enames.index("SCI")
     if all(flag == True for fi,flag in extens.items()) == True:
         return infile
     else:
         # create new name
         dfsplit = infile.split('/')
-        #newfile = dfsplit[0:-1]+'/'+dfsplit[1].split('.fits')[0]+'_vardq.fits'
         newfile = '/'.join(dfsplit[:-1])+'/'+dfsplit[-1].split('.fits')[0]+'_vardq.fits'
         if not os.path.isfile(newfile):
             shutil.copy(infile,newfile)
             print('create',newfile)
-        dataIN, hdrIN = fits.getdata(infile, 0, header=True)
-        flx = dataIN
-        hdu_0 = fits.PrimaryHDU(flx,header=hdrIN)
-        hdul = fits.HDUList([hdu_0])
+        hdul = fits.open(newfile,mode='update')
+        datIN = hdul[sciEXT].data
         for ext in extens:
-            print(ext,extens[ext])
             if ext == 'VAR' and extens[ext] == False:
-                var = copy.deepcopy(flx)
+                var = copy.deepcopy(datIN)
                 hdu_a = fits.ImageHDU(var,name='var')
                 hdul.append(hdu_a)
             elif ext == 'DQ' and extens[ext] == False:
-                dq  = np.zeros(dataIN.shape)
+                dq  = np.zeros(datIN.shape)
                 hdu_b = fits.ImageHDU(dq,name='dq')
                 hdul.append(hdu_b)
-        hdul.writeto(newfile,overwrite=CLOBBER)
+        hdul.writeto(newfile,overwrite=CLOBBER,output_verify='ignore')
         hdul.close()
         return newfile

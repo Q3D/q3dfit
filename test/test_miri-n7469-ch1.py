d77ae0414f93e3f2543d50f0ae484620e1cf9a0b
David Rupke <drupke@gmail.com>
2022-10-03 21:07:00 -0500

    Progress on fit; renormalization and powerlaw component, plus no qso decomp

---
+++ test/test_miri-n7469-ch1.py
@@ -0,0 +1,13 @@
+from os import chdir
+from q3dfit.q3df import q3df
+from q3dfit.q3da import q3da
+
+chdir('../jnb/')
+
+q3di = 'miri-n7469-ch1/q3di.npy'
+
+cols = 23
+rows = 24
+
+q3df(q3di, cols=cols, rows=rows, quiet=False)
+q3da(q3di, cols=cols, rows=rows)

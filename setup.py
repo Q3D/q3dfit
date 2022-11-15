ae2be08f6e43f947e1c9fa3b8f9bc2544eb50b32
David Rupke <drupke@gmail.com>
2022-11-14 20:55:05 -0600

    remove beta from version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='q3dfit',
-    version='0.1.0-beta',    
+    version='0.1.0',    
     url='https://q3dfit.readthedocs.io/en/latest/index.html',
     author='David Rupke',
     author_email='drupke@gmail.com',

Enforcing float32 for all fit inputs to prevent numerical precision errors. Moved linelist tables to float32. Fixed q3dpro [OI]/Ha error. PG1411 cube now fits through.

q3dfit/fitloop.py
@@ -155,7 +155,8 @@ def fitloop(ispax, colarr, rowarr, cube, q3di, listlines, specConv,
                     for line in q3di.lines:
                         listlinesz[line] = \
                             np.array(listlines['lines']
-                                     [(listlines['name'] == line)]) * \
+                                     [(listlines['name'] == line)],
+                                     dtype='float32') * \
                                 (1. + q3di.zinit_gas[line][i, j, ])
 
             if q3di.docontfit:

q3dfit/fitspec.py
@@ -115,7 +115,7 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
 # ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     if q3di.fitrange is not None:
-        fitran_tmp = q3di.fitrange
+        fitran_tmp = list(map(np.float32, q3di.fitrange))
     else:
         fitran_tmp = [wlambda[0], wlambda[len(wlambda)-1]]
     # indices locating good data and data within fit range
@@ -134,7 +134,8 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
                                           gd_indx_8, gd_indx_9)
     gd_indx_full = list(gd_indx_full)
     # limit actual fit range to good data
-    fitran = [np.min(wlambda[gd_indx_full]), np.max(wlambda[gd_indx_full])]
+    fitran = [np.min(wlambda[gd_indx_full]).astype('float32'),
+              np.max(wlambda[gd_indx_full]).astype('float32')]
 
     # indices locating data within actual fit range
     fitran_indx1 = np.where(wlambda >= fitran[0])[0]
@@ -407,7 +408,7 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
     if q3di.dolinefit:
 
         q3do.init_linefit(listlines, q3di.lines, q3di.maxncomp,
-                          line_dat=continuum)
+                          line_dat=continuum.astype('float32'))
 
         # Check that # components being fit to at least one line is > 0
         nonzerocomp = np.where(np.array(list(ncomp.values())) != 0)[0]
@@ -482,13 +483,16 @@ def fitspec(wlambda, flux, err, dq, zstar, listlines, listlinesz, ncomp,
                                 np.where(peakinit[line] < 0., 0., peakinit[line])
                         else:
                             peakinit[line] = np.zeros(q3di.maxncomp)
+                        # re-cast as float32
+                        peakinit[line] = peakinit[line].astype('float32')
 
             # Initial guesses for emission line widths
             if siginit_gas is None:
                 siginit_gas = {k: None for k in q3di.lines}
                 for line in q3di.lines:
                     siginit_gas[line] = \
-                        np.zeros(q3di.maxncomp) + siginit_gas_def
+                        np.zeros(q3di.maxncomp, dtype='float32') + \
+                        np.float32(siginit_gas_def)
 
             # Fill out parameter structure with initial guesses and constraints
             impModule = import_module('q3dfit.' + q3di.fcnlineinit)

q3dfit/lineinit.py
@@ -6,6 +6,7 @@ from astropy.table import QTable, Table
 from lmfit import Model
 from q3dfit.q3dutil import lmlabel
 from q3dfit.exceptions import InitializationError
+import copy
 import numpy as np
 import q3dfit.data
 import os
@@ -18,7 +19,11 @@ def lineinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp, s
     '''
     # Get fixed-ratio doublet pairs for tying intensities
     data_path = os.path.abspath(q3dfit.data.__file__)[:-11]
-    doublets = Table.read(data_path+'linelists/doublets.tbl', format='ipac')
+    doublets64 = Table.read(data_path+'linelists/doublets.tbl', format='ipac')
+    doublets = \
+        Table(doublets64,
+              dtype=['str', 'str', 'int', 'float32', 'float32', 'float32'])
+
     dblt_pairs = dict()
     for idx, name in enumerate(doublets['line1']):
         if doublets['fixed_ratio'][idx] == 1:
@@ -31,7 +36,7 @@ def lineinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp, s
     #    specres = np.float32(specres)
     # A reasonable lower limit of 5d for physicality
     if siglim is None:
-        siglim = np.array([5., 2000.])
+        siglim = np.array([5., 2000.], dtype='float32')
     else:
         siglim = np.array(siglim, dtype='float32')
 
@@ -51,7 +56,7 @@ def lineinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp, s
             # LMFIT parameters can only consist of letters,  numbers, or _
             lmline = lmlabel(line)
             mName = f'{lmline.lmlabel}_{i}_'
-            imodel = Model(manygauss, prefix=mName,SPECRES=specConv)
+            imodel = Model(manygauss, prefix=mName, SPECRES=specConv)
             if isinstance(totmod, Model):
                 totmod += imodel
             else:
@@ -120,7 +125,7 @@ def lineinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp, s
 #            if not inrange:
 #                vary = False
         else:
-            value = 0.  # specres
+            value = np.float32(0.)
             limited = None
             limits = None
             vary = False
@@ -160,7 +165,7 @@ def lineinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp, s
                                 fit_params[f'{lmline1.lmlabel}_{comp}_flx'],
                                 fit_params[f'{lmline2.lmlabel}_{comp}_flx'])
                     lmrat = f'{lmline1.lmlabel}_div_{lmline2.lmlabel}_{comp}'
-                    fit_params.add(lmrat, value=initval)
+                    fit_params.add(lmrat, value=initval.astype('float32'))
                     # tie second line to first line divided by the ratio
                     fit_params[f'{lmline2.lmlabel}_{comp}_flx'].expr = \
                         f'{lmline1.lmlabel}_{comp}_flx'+'/'+lmrat
@@ -171,34 +176,34 @@ def lineinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp, s
                     # apply lower limit?
                     if 'lower' in lineratio.colnames:
                         lower = lineratio['lower'][ilinrat]
-                        fit_params[lmrat].min = lower
+                        fit_params[lmrat].min = lower.astype('float32')
                     # logic to apply doublet lower limits if in doublets table
                     elif line1 in doublets['line1']:
                         iline1 = np.where(doublets['line1'] == line1)
                         if doublets['line2'][iline1] == line2:
                             lower = doublets['lower'][iline1][0]
-                        fit_params[lmrat].min = lower
+                        fit_params[lmrat].min = lower.astype('float32')
                     # doublet can be specified in init file in either order
                     # relative to doublets table ...
                     elif line1 in doublets['line2']:
                         iline1 = np.where(doublets['line2'] == line1)
                         if doublets['line1'][iline1] == line2:
                             upper = 1. / doublets['lower'][iline1][0]
-                        fit_params[lmrat].max = upper
+                        fit_params[lmrat].max = upper.astype('float32')
                     # apply upper limit?
                     if 'upper' in lineratio.colnames:
                         upper = lineratio['upper'][ilinrat]
-                        fit_params[lmrat].max = upper
+                        fit_params[lmrat].max = upper.astype('float32')
                     elif line1 in doublets['line1']:
                         iline1 = np.where(doublets['line1'] == line1)
                         if doublets['line2'][iline1] == line2:
                             upper = doublets['upper'][iline1][0]
-                        fit_params[lmrat].max = upper
+                        fit_params[lmrat].max = upper.astype('float32')
                     elif line1 in doublets['line2']:
                         iline1 = np.where(doublets['line2'] == line1)
                         if doublets['line1'][iline1] == line2:
                             lower = 1. / doublets['upper'][iline1][0]
-                        fit_params[lmrat].min = lower
+                        fit_params[lmrat].min = lower.astype('float32')
 
     # pass siglim_gas back because the default is set here, and it's needed
     # downstream
@@ -207,16 +212,19 @@ def lineinit(linelist, linelistz, linetie, initflux, initsig, maxncomp, ncomp, s
 
 def set_params(fit_params, NAME, VALUE=None, VARY=True, LIMITED=None,
                TIED=None, LIMITS=None):
+    # we can force the input to float32, but lmfit has values as float64 and
+    # doesn't seem like we can change it. These astypes assume that the
+    # VALUE is a numpy object
     if VALUE is not None:
-        fit_params[NAME].set(value=VALUE)
+        fit_params[NAME].set(value=VALUE.astype('float32'))
     fit_params[NAME].set(vary=VARY)
     if TIED is not None:
         fit_params[NAME].expr = TIED
     if LIMITED is not None and LIMITS is not None:
         if LIMITED[0] == 1:
-            fit_params[NAME].min = LIMITS[0]
+            fit_params[NAME].min = LIMITS[0].astype('float32')
         if LIMITED[1] == 1:
-            fit_params[NAME].max = LIMITS[1]
+            fit_params[NAME].max = LIMITS[1].astype('float32')
     return fit_params
 
 

q3dfit/q3din.py
@@ -124,9 +124,11 @@ class q3din:
         # check for defined zsys
         if zinit is None:
             if self.zsys_gas is not None:
-                zinit = self.zsys_gas
+                zinit = np.float32(self.zsys_gas)
             else:
                 print('problem!')
+        else:
+            zinit = np.float32(zinit)
 
         self.lines = lines
 
@@ -169,11 +171,13 @@ class q3din:
         for i, line in enumerate(self.lines):
             self.linetie[line] = linetie[i]
             self.ncomp[line] = np.full((self.ncols, self.nrows),
-                                       self.maxncomp)
+                                       self.maxncomp, dtype='int')
             self.zinit_gas[line] = np.full((self.ncols, self.nrows,
-                                            self.maxncomp), zinit)
+                                            self.maxncomp), zinit,
+                                           dtype='float32')
             self.siginit_gas[line] = np.full((self.ncols, self.nrows,
-                                              self.maxncomp), siginit)
+                                              self.maxncomp), siginit,
+                                             dtype='float32')
 
     def init_contfit(self, fcncontfit, siginit=50., zinit=None, argscontfit={},
                      argscontplot={}, argsconvtemp={},
@@ -261,9 +265,11 @@ class q3din:
         # check for defined zsys
         if zinit is None:
             if self.zsys_gas is not None:
-                zinit = self.zsys_gas
+                zinit = np.float32(self.zsys_gas)
             else:
                 print('problem!')
+        else:
+            zinit = np.float32(zinit)
 
         # check that load_cube() has been invoked, or ncols/nrows otherwise
         # defined
@@ -271,8 +277,10 @@ class q3din:
             _ = self.load_cube(self.infile)
             print('q3di: Loading cube to get ncols, nrows')
 
-        self.siginit_stars = np.full((self.ncols, self.nrows), siginit)
-        self.zinit_stars = np.full((self.ncols, self.nrows), zinit)
+        self.siginit_stars = np.full((self.ncols, self.nrows), siginit,
+                                     dtype='float32')
+        self.zinit_stars = np.full((self.ncols, self.nrows), zinit,
+                                   dtype='float32')
 
     def load_cube(self):
         if not os.path.isfile(self.infile):
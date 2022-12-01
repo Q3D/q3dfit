3646ef098272acf97de00e3256d9ef4da4506a1b
David Rupke <drupke@gmail.com>
2022-12-01 10:47:35 -0600

    Successfully fitted both example data cubes. Test notebooks run to completion. Includes bug fix in set_figSize function in q3dpro, as well as tick mark bugfix in linemaps.

--- test/test_nirspec-j1652.py
+++ test/test_nirspec-j1652.py
@@ -11,12 +11,12 @@ chdir('../jnb/')
 q3di = 'nirspec-j1652/q3di.npy'
 
 # Single spaxel
-cols=45
-rows=29
+#cols=45
+#rows=29
 
 # Run the fit
-from q3dfit.q3df import q3dfit
-q3dfit(q3di, cols=cols, rows=rows, quiet=False)
+#from q3dfit.q3df import q3dfit
+#q3dfit(q3di, cols=cols, rows=rows, quiet=False)
 
 #
 # Plot an example
@@ -40,7 +40,14 @@ q3dfit(q3di, cols=cols, rows=rows, quiet=False)
 # q3do.plot_cont(q3di, plotargs=argscontplot)
 
 # Most of cube
-# q3dfit(q3di, cols=[20,75], rows=[15,70], ncores=10)
+#from q3dfit.q3df import q3dfit
+#q3dfit(q3di, cols=[20,75], rows=[15,70], ncores=10)
+
+# Collate data
+#cols = [20, 75]
+#rows = [15, 70]
+#from q3dfit.q3dcollect import q3dcollect
+#q3dcollect(q3di, cols=cols, rows=rows)
 
 # Make maps
 #
@@ -54,18 +61,18 @@ q3dfit(q3di, cols=cols, rows=rows, quiet=False)
 #o3data.calc_cvdf(2.9489, [-5e3, 5e3], vstep=5)
 
 # test plot of CVDF calc
-# import matplotlib.pyplot as plt
-# fig, ax = plt.subplots()
-# ax.step(o3data.cvdf_vel, o3data.vdf[49, 39, :])
+#import matplotlib.pyplot as plt
+#fig, ax = plt.subplots()
+#ax.step(o3data.cvdf_vel, o3data.vdf[49, 39, :])
 
 # compute v50 for [OIII] test
-# v50 = o3data.calc_cvdf_vel(68.)
-# import matplotlib.pyplot as plt
-# from matplotlib import cm
-# norm = cm.colors.Normalize(vmax=2e3, vmin=-2e3)
-# fig, ax = plt.subplots()
-# ax.imshow(v50, norm=norm, cmap='RdYlBu')
-# plt.show()
+#v50 = o3data.calc_cvdf_vel(68.)
+#import matplotlib.pyplot as plt
+#from matplotlib import cm
+#norm = cm.colors.Normalize(vmax=2e3, vmin=-2e3)
+#fig, ax = plt.subplots()
+#ax.imshow(v50, norm=norm, cmap='RdYlBu')
+#plt.show()
 
 # map of v50 for [OIII] test
 # o3data.make_cvdf_map(50., velran=[-1e3, 1e3], markcenter=[0., 0.],

3010f4d77d0348b48d98fabe3a260b3b6c649f3a
David Rupke <drupke@gmail.com>
2024-10-11 15:04:14 -0500

    deleting test scripts

--- test/test_nirspec-etc.py
+++
@@ -1,9 +0,0 @@
-from os import chdir
-from q3dfit.q3df import q3df
-from q3dfit.q3da import q3da
-
-chdir('../jnb/')
-q3di = 'nirspec-etc/q3di.npy'
-
-q3df(q3di, cols=13, rows=20, quiet=False)
-q3da(q3di, cols=13, rows=20)
